e595e526082cd565394608aa90c8628cb41cd520
LuisDaSilva <luisdasilvads@gmail.com>
2019-07-08 00:38:05 -0400

    Agregando login y registro

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Ingresar - InvestigaUCAB</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{URL::asset('assets/app/images/logo-ucab-icono.png')}}">
+
+        <!-- App css -->
+        <link href="{{URL::asset('assets/app/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{URL::asset('assets/app/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{URL::asset('assets/app/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="authentication-bg authentication-bg-pattern">
+
+        <div class="account-pages mt-5 mb-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card bg-pattern">
+
+                            <div class="card-body p-4">
+                                
+                                <div class="text-center w-75 m-auto">
+                                    <a href="index.html">
+                                        <span><img src="{{URL::asset('assets/app/images/logo-dark-large.png')}}" alt="" height="50"></span>
+                                    </a>
+                                    <p class="text-muted mb-4 mt-3">Ingresa tu correo y tu contraseña para ingresar al panel administrativo.</p>
+                                </div>
+
+                                <form action="#">
+
+                                    <div class="form-group mb-3">
+                                        <label for="emailaddress">Correo</label>
+                                        <input class="form-control" type="email" id="emailaddress" required="" placeholder="Ingresa tu correo">
+                                    </div>
+
+                                    <div class="form-group mb-3">
+                                        <label for="password">Contraseña</label>
+                                        <input class="form-control" type="password" required="" id="password" placeholder="Ingresa tu contraseña">
+                                    </div>
+
+                                    <div class="form-group mb-3">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="checkbox-signin" checked>
+                                            <label class="custom-control-label" for="checkbox-signin">Recordarme</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group mb-0 text-center">
+                                        <button class="btn btn-primary btn-block" type="submit"> Ingresar </button>
+                                    </div>
+
+                                </form>
+                            </div> <!-- end card-body -->
+                        </div>
+                        <!-- end card -->
+
+                        <div class="row mt-3">
+                            <div class="col-12 text-center">
+                                <p> <a href="pages-recoverpw.html" class="text-white-50 ml-1">Olvidaste tu contraseña?</a></p>
+                                <p class="text-white-50">No tienes cuenta? Crea una!<a href="{{route('register')}}" class="text-white ml-1"><b>Registrarse</b></a></p>
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end page -->
+        <!-- Vendor js -->
+        <script src="{{URL::asset('assets/app/js/vendor.min.js')}}"></script>
+
+        <!-- App js -->
+        <script src="{{URL::asset('assets/app/js/app.min.js')}}"></script>
+        
+    </body>
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Resgistro - InvestigaUCAB</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{URL::asset('assets/app/images/logo-ucab-icono.png')}}">
+
+        <!-- App css -->
+        <link href="{{URL::asset('assets/app/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{URL::asset('assets/app/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{URL::asset('assets/app/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="authentication-bg authentication-bg-pattern">
+
+        <div class="account-pages mt-5 mb-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card bg-pattern">
+
+                            <div class="card-body p-4">
+                                
+                                <div class="text-center w-75 m-auto">
+                                    <a href="index.html">
+                                        <span><img src="{{URL::asset('assets/app/images/logo-dark-large.png')}}" alt="" height="50"></span>
+                                    </a>
+                                    <p class="text-muted mb-4 mt-3">Todavia no tienes cuenta? Crea tu cuenta, toma menos de un minuto</p>
+                                </div>
+
+                                <form action="#">
+
+                                    <div class="form-group">
+                                        <label for="fullname">Nombre Completo</label>
+                                        <input class="form-control" type="text" id="fullname" placeholder="Ingresa tu nombre" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="emailaddress">Correo Electronico</label>
+                                        <input class="form-control" type="email" id="emailaddress" required placeholder="Ingresa tu correo">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">Contraseña</label>
+                                        <input class="form-control" type="password" required id="password" placeholder="Ingresa tu contraseña">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="checkbox-signup">
+                                            <label class="custom-control-label" for="checkbox-signup">Acepto <a href="javascript: void(0);" class="text-dark">los terminos y condiciones</a></label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-0 text-center">
+                                        <button class="btn btn-success btn-block" type="submit"> Ingresar </button>
+                                    </div>
+
+                                </form>
+
+                                <div class="text-center">
+                                    <h5 class="mt-3 text-muted">Ingresar usando</h5>
+                                    <ul class="social-list list-inline mt-3 mb-0">
+                                        <li class="list-inline-item">
+                                            <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github-circle"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </div> <!-- end card-body -->
+                        </div>
+                        <!-- end card -->
+
+                        <div class="row mt-3">
+                            <div class="col-12 text-center">
+                                <p class="text-white-50">Ya tienes cuenta?  <a href="{{route('login')}}" class="text-white ml-1"><b>Ingresa</b></a></p>
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end page -->
+
+        <footer class="footer footer-alt">
+            2015 - 2019 &copy; UBold theme by <a href="" class="text-white-50">Coderthemes</a> 
+        </footer>
+
+        <!-- Vendor js -->
+        <script src="{{URL::asset('assets/app/js/vendor.min.js')}}"></script>
+
+        <!-- App js -->
+        <script src="{{URL::asset('assets/app/js/app.min.js')}}"></script>
+        
+    </body>
+</html>
